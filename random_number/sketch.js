4d02db0594f2d95985f00ee61af1bbe3294ce753
DaNagi88 <gyxcyh5g@gmail.com>
2022-07-03 14:43:03 +0900

    refactor random

--- random_number/sketch.js
+++ random_number/sketch.js
@@ -13,14 +13,13 @@ function setup() {
     for (i=0; i<1000; i++) {
         fill(random(255), random(255), random(255), 150)
 
-        r = random(sqrt(width**2+height**2)/2-offset)
+        src_r = random(sqrt(width**2+height**2)/2-offset)
         src_phi = random(0, PI)
 
-        phi = int(src_phi/(PI/section))*(PI/section)*2 + src_phi%(PI/section)
-        os_phi = phi+r/100
-        ellipse(
-            (r+offset)*cos(os_phi), -(r+offset)*sin(os_phi), r/5
-        )
+        phi = int(src_phi/(PI/section))*(PI/section)*2 + src_phi%(PI/section)+src_r/100
+        r = src_r+offset
+        diam = src_r/7
+        ellipse(r*cos(phi), -r*sin(phi), diam)
     }
   }
    
