02f4e3abb0b2877b3efad16be2ee48bbe235ab74
DaNagi88 <gyxcyh5g@gmail.com>
2022-07-03 14:34:58 +0900

    update random number

--- random_number/sketch.js
+++ random_number/sketch.js
@@ -6,15 +6,21 @@ function setup() {
 
     //noFill()
     noStroke()
+
+    offset = 30
+    section = 8
+
     for (i=0; i<1000; i++) {
         fill(random(255), random(255), random(255), 150)
-        offset = 50
-        r = random(sqrt(width**2+height**2)/2-offset)
-        phi = random(-PI, PI)
-        ellipse(
-            (r+offset)*cos(phi), (r+offset)*sin(phi), r/5
-        )
 
+        r = random(sqrt(width**2+height**2)/2-offset)
+        src_phi = random(0, PI)
+
+        phi = int(src_phi/(PI/section))*(PI/section)*2 + src_phi%(PI/section)
+        os_phi = phi+r/100
+        ellipse(
+            (r+offset)*cos(os_phi), -(r+offset)*sin(os_phi), r/5
+        )
     }
   }
    
